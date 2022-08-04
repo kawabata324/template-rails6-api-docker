a887245ccef3de26cc1d40e5f15992131ff43277
kawabata <s1931115@s.konan-u.ac.jp>
2022-08-05 02:39:26 +0900

    [Update] test messageを受け取る

--- app/controllers/test/tests_controller.rb
+++ app/controllers/test/tests_controller.rb
@@ -3,4 +3,10 @@ class Test::TestsController < ApplicationController
     messages = Message.all
     render json: {messages: messages, status: 200}
   end
+
+  def create
+    message = params[:message]
+    createdMessage = Message.create!({ body: message })
+    render json: {status: 200, message: createdMessage}
+  end
 end
