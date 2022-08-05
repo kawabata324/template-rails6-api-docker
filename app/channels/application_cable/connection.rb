f52894de96b928fbdb8b3a6c9a960b2455f51187
kawabata <s1931115@s.konan-u.ac.jp>
2022-08-05 10:24:33 +0900

    [Update] chanelの作成 追加methodの作成 配信methodの作成

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,10 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :chat_token
+
+    def connect
+      self.chat_token = Time.now.to_i
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     get 'tests/index'
     post 'tests', to: 'tests#create'
   end
+
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
